1c084cef3fffcbbd40d97f38164cd487efc1a72a
Moh Rizky Dwi Jayanto <109938825+R1nto@users.noreply.github.com>
2022-12-04 23:08:31 +0700

    Add files via upload

---
+++ belajar-PHP/index.php
@@ -0,0 +1,26 @@
+halaman index
+<?php
+    $judul="Belajar PHP";
+    $isi="SMKN 2 Buduran";
+    $Jurusan="Rekayasa Perangkat Lunak";
+    $kelas="11";
+    $nama="Rinto";
+    echo $judul;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo $nama;
+        ?>
+    </h1>
+</body>
+</html>
